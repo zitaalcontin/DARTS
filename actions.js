b3cc4c0613facbb2daf7f5e7a92bc270901a571c
Ariane Villalino <arianevillalino@gmail.com>
2024-02-19 20:43:52 +0800

    add: super admin and other folders

---
+++ actions.js
@@ -0,0 +1,11 @@
+const ToSA = ()=>{
+    window.location.href = "./SuperAdmin/html/map.html"
+    alert("hi");
+}
+
+const ToBrgy = ()=>{
+    window.location.href = "SuperAdmin/html/map.html"
+}
+const ToMSWDO = ()=>{
+    window.location.href = "SuperAdmin/html/map.html"
+}
